29556cdd1ac3eb1fc12d278d851292a506e4a38a
Sean Tatu <statu001@my.hennepintech.edu>
2015-10-24 20:10:25 -0500

    updated scripts.

--- js/script.js
+++ js/script.js
@@ -1,46 +1,79 @@
 
 (function () {
-    var kevinSmithMovies = {
+    
        var data = null;
 
     var object = kevinSmithMovies.items;
-    var showMovies = document.getElementById("movies")[0];
+    var localStorageKey = 
+    var target = document.getElementById("movies");
     moviesCount = object.length;
     target = document.getElementById("movies");
-    if (moviesCount > 0) {
-        for (var i = 0; i < moviesCount; i++) {
-            var item = object[i];
-            var title = item.title;
-            var director = item.director;
-            var producer = item.producer;
-            var year = item.year;
-            var cast = item.cast;
-            var h3 = document.createElement("h3");
-            
-            var headingText = document.createTextNode(title);
-            h3.appendChild(headingText);
-            target.appendChild(h3);
-
-            var ul = document.createElement("ul");
-            var stats = document.createTextNode("Director: " + director + ", "  + "Producer: " + producer + ", " + "Released: " + year + ", " + "Cast: " + cast);
-            ul.appendChild(stats);
-            target.appendChild(ul);
+    var ajax = document.getElementById("ajax"); 
+    var load = document.getElementById("load"); 
+    var save = document.getElementById("save"); 
+    var clear = document.getElementById("clear");
+    
+    
+    var headingText = document.createTextNode(title);
+    var h3.appendChild(headingText);
+    var target.appendChild(h3);
+    var ul = document.createElement("ul");
+    var stats = document.createTextNode("Director: " + director + ", "  + "Producer: " + producer + ", " + "Released: " + year + ", " + "Cast: " + cast);
+        ul.appendChild(stats);
+        target.appendChild(ul);
         }
     }
+    function showMovies() {
+        var moviesCount = data.kevinSmithMovies;
+        var i, movie;
+    for (var i = 0; i < moviesCount; i++) {
+            target.appendChild"ul";
+        movie = moviesCount[i];
+    }
+}
+
+    
+    
+    function getHTTPObject(){
+        var xhr;
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        }else if (window.ActiveXObject) {
+            xhr = new ActiveXObject("Msxm112.XMLHTTP");
+        }
+            return xhr;
+        }
+    
+        var request = getHTTPObject();
+        request.onreadystatechange = function(){
+        if(request.readyState === 4 && request.status === 200) {
+            
+            text = request.responseText; 
+               data = JSON.parse(text); 
+                
+               showMovies();
+               ul.appendChild(data);
+               target.appendChild(ul);
+        }
+    }
+    request.open("GET", "data/kevinSmithMovies.json", true);
+    request.send(null);
+    
 function loadLocalData() { 
         if (typeof(localStorage) === 'undefined') { 
-    targetArea.innerHTML = "Browser does not support local storage."; 
+    target.innerHTML = "Browser does not support local storage."; 
          } 
        else { 
           // Do the stuff to load the page data 
-             targetArea.innerHTML = "Loading Data..."; 
+             target.innerHTML = "Loading Data..."; 
            text = localStorage.getItem(localStorageKey); 
             if (text === null) { 
-                targetArea.innerHTML = "No local data found."; 
+                target.innerHTML = "No local data found."; 
            }  
            else { 
                data = JSON.parse(text); 
-               showTedTalkData(data); 
+               showMovies(data);
+               
            } 
         } 
     } 
@@ -48,11 +81,11 @@ function loadLocalData() {
     
    function saveDataLocally(){ 
          if (typeof(localStorage) === 'undefined') { 
-            targetArea.innerHTML = "Browser does not support local storage."; 
+            target.innerHTML = "Browser does not support local storage."; 
        } 
         else { 
            if (data === null) { 
-               targetArea.innerHTML = "Data must be loaded before it can be saved."; 
+               target.innerHTML = "Data must be loaded before it can be saved."; 
             }  
              else { 
                 localStorage.setItem(localStorageKey, JSON.stringify(data)); 
@@ -62,21 +95,20 @@ function loadLocalData() {
                                   
    function clearDataLocally(){ 
        if (typeof(localStorage) === 'undefined') { 
-            targetArea.innerHTML = "Browser does not support local storage."; 
+            target.innerHTML = "Browser does not support local storage."; 
         } 
         else { 
            localStorage.removeItem(localStorageKey); 
        } 
    } 
-    
-    // Do the stuff to load the page data 
-    targetArea.innerHTML = "Click a button to Load Data"; 
+
+    target.innerHTML = "Click a button to Load Data"; 
  
  
-    ajax.addEventListener("click", loadDataAjax, false); 
-    load.addEventListener("click", loadLocalData, false); 
-     save.addEventListener("click", saveDataLocally, false); 
-    clear.addEventListener("click", clearDataLocally, false);  
+    ajax.addEventListener("click", loadDataAjax, true); 
+    load.addEventListener("click", loadLocalData, true); 
+     save.addEventListener("click", saveDataLocally, true); 
+    clear.addEventListener("click", clearDataLocally, true);  
 
 
 
