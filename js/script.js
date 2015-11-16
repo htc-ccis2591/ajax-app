9003ea5ee7f2c66039f3b8a4333b7d1dcecea024
Sean Tatu <statu001@my.hennepintech.edu>
2015-11-15 22:51:48 -0600

    escaped quotes and removed(data)

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@
     $(document).ready(function () {
 
         var kevinSmithApp = {
-            loadContent: function (data) {
+            loadContent: function () {
                 var h1 = $("#h1");
-                $.getJSON('data/kevinSmithMovies.json', function (data) {
+                $.getJSON('data/kevinSmithMovies.json', function () {
                     var titles = data.titles,
                         count = titles.length;
-                    $("<h2 id="h2">I exist</h2>").insertAfter(h1);
-                    $('<ul id="ul">').insertAfter(h2.children().last());
+                    $("<h2 id=\"h2\">I exist</h2>").insertAfter(h1);
+                    $('<ul id=\"ul\">').insertAfter(h2.children().last());
                     if (count > 0) {
-                        $.each(titles, function (i, obj) {
+                        $.each(titles, function (i) {
                             $('#ul').append('<li>' + '<span>' + obj.title + '' + obj.director + '' + obj.producer + '' + obj.year + '' + obj.cast + '</span>' + '</li>');
                         });
                     }
