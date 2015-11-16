3390e9dee51bffa90143d9c6cb7a0e328b769a07
Sean Tatu <statu001@my.hennepintech.edu>
2015-11-16 00:24:41 -0600

    successful ajax call. no data on page

--- js/script.js
+++ js/script.js
@@ -5,20 +5,27 @@
         var kevinSmithApp = {
             loadContent: function () {
                 var h1 = $("#h1");
-                $.getJSON('data/kevinSmithMovies.json', function () {
+                $('<ul id=\"ul\">').insertAfter(h1.children().last());
+                $.getJSON('data/kevinSmithMovies.json', function (data) {
                     var titles = data.titles,
                         count = titles.length;
-                    $("<h2 id=\"h2\">I exist</h2>").insertAfter(h1);
-                    $('<ul id=\"ul\">').insertAfter(h2.children().last());
                     if (count > 0) {
                         $.each(titles, function (i) {
-                            $('#ul').append('<li>' + '<span>' + obj.title + '' + obj.director + '' + obj.producer + '' + obj.year + '' + obj.cast + '</span>' + '</li>');
+                            $('#ul').append('<li>' + '<span>' + titles.title + '' + titles.director + '' + titles.producer + '' + titles.year + '' + titles.cast + '</span>' + '</li>');
                         });
                     }
 
+                }).error(function() {
+                    alert('unable to load titles');
+                }).complete(function() {
+                    alert('ajax call completed');
+                }).success(function() {
+                    alert('your ajax call was a success');
                 });
+                
             }
         }
  kevinSmithApp.loadContent();
-    });
+
+        });
 }())
