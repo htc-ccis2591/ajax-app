4c853470f2aa9088e891f311369333f5e08b72f3
JoeFranks1993 <Josephdeanfranks@gmail.com>
2015-11-16 20:37:48 -0600

    jqueryified My first app
    jqueryified My first app

--- Script.js
+++ Script.js
@@ -1,130 +1,128 @@
-﻿var inventory = document.getElementById("inventory");
-var Liquerinventory = null;
-var getfield = document.getElementById("inventorydropdown");
-var itemheader = document.getElementById("itemheader");
-var cost = document.getElementById("cost");
-var proof = document.getElementById("proof");
-var imgholder = document.getElementById("imgholder");
-var favbutton = document.getElementById("favbutton");
-var favoritearray = [];
-var favoritesdisplay = document.getElementById("favoritesdisplay")
-var savebutton = document.getElementById("savebutton")
+﻿$(document).ready(function () {
 
-// Setting What Object To Use For AJAX
-function getHTTPObject() {
-    
-    var xhr;
+    var Liquerinventory = null;
+    var $clearbutton = $("#clearbutton")
+    var $loadbutton = $("#loadbutton")
+    var $favbutton = $("#favbutton");
+    var $getfield = $("#inventorydropdown");
+    var $itemheader = $("#itemheader");
+    var $cost = $("#cost");
+    var $proof = $("#proof");
+    var $imgholder = $("#imgholder");
+    var favoritearray = [];
+    var counter = -1;
+    var $favoritesdisplay = $("#favoritesdisplay");
+    var $tnimageholder = $("#holder");
 
-    if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-    }
-    else if (window.ActiveXObject) {
-        xhr = new ActiveXObject("Msxml2.XMLHTTP");
-    }
+    // Event Listener For Drop Down Field Changes
+    $getfield.on("change", function () {
+        viewinventory($getfield.val())
+    })
+    //Event Listener For Buttons 
+    $favbutton.on("click", addfav)
+    $clearbutton.on("click", clearfavorites)
+    $loadbutton.on("click", loadfavorites)
 
-    return xhr;
-}
-// Making The AJAX Request, Waiting For The Data To Be Ready, Parsing The String Returned Into a JSON.  
-// Invoking "setdropdown" Function.
-function loaddata() {
-   
-    var request = getHTTPObject();
-    request.open("GET", "data/liquers.json", true);
-    request.send(null);
-    request.onreadystatechange = function () {
-        var text;
-        if (request.readyState === 4 && request.status === 200) {
-            text = request.responseText;
-            Liquerinventory = JSON.parse(text);
-          //  display();
+
+    $.ajax({
+        type: 'GET',
+        url: "data/liquers.json",
+        dataType: "json",
+        success: function (data) {
+            Liquerinventory = data; 
             setdropdown();
         }
-    }
-}
-loaddata()
-
-// Change Displayed Data Based On Selected Dropdown Item.
-function viewinventory(item) {
+    })
    
-    for (i = 0; i < Liquerinventory.Liquers.length; i++) {
-        if (Liquerinventory.Liquers[i].description == item) {
-            itemdetails = (Liquerinventory.Liquers[i])
-            itemheader.innerHTML = itemdetails.description;
-            cost.innerHTML = "Cost" + " " + "$" + itemdetails.cost;
-            proof.innerHTML = "proof =" + " " + itemdetails.proof;
-            imgholder.style.backgroundImage = "url(" + itemdetails.image + ")";
+    // Change Displayed Data Based On Selected Dropdown Item.
+    function viewinventory(item) {
+
+        for (i = 0; i < Liquerinventory.Liquers.length; i++) {
+
+            if (Liquerinventory.Liquers[i].description == item) {
+                tnimageholder = Liquerinventory.Liquers[i].tnimage;
+                itemdetails = (Liquerinventory.Liquers[i])
+                $itemheader.html(itemdetails.description);
+                $cost.html("Cost" + " " + "$" + itemdetails.cost);
+                $proof.html("proof =" + " " + itemdetails.proof);
+                $imgholder.css("background-image", "url(" + itemdetails.image + ")");
+                $tnimageholder.css("background-image", "url(" + itemdetails.tnimage + ")");
+            }
+            else if (item == "") {
+                $itemheader.html("");
+                $cost.html("");
+                $proof.html("");
+                $imgholder.css("background-image", "url('')");
+            }
         }
-        else if (item == "") {
-            itemheader.innerHTML = ""
-            cost.innerHTML = ""
-            proof.innerHTML = ""
-            imgholder.style.backgroundImage = ""
+    }
+    // Loads All Item Descriptions Into The Dropdown Bar.
+    function setdropdown() {
+        for (i = 0; i < Liquerinventory.Liquers.length; i++) {
+            $getfield.append("<option>" + Liquerinventory.Liquers[i].description + "</option>")
         }
     }
-}
-// Loads All Item Descriptions Into The Dropdown Bar.
-function setdropdown() {
-    
-    for (i = 0; i < Liquerinventory.Liquers.length; i++){
-        var addchoice = document.createElement("option")
-        addchoice.innerHTML = Liquerinventory.Liquers[i].description;
-        getfield.add(addchoice);
-    }
-}
 
 
-// Event Listener For Drop Down Field Changes
-getfield.addEventListener("change", function() {
-    console.log(getfield.children[getfield.selectedIndex].text);
-    viewinventory(getfield.children[getfield.selectedIndex].text)
-})
 
-//Event Listener For Buttons 
-favbutton.addEventListener("click", addfav)
-clearbutton.addEventListener("click", clearfavorites)
-loadbutton.addEventListener("click", loadfavorites)
 
-// Storing favorite data into a JSON
-function addfav() {
+    // Storing favorite data into a JSON
+    function addfav() {
+        if ($getfield.val() != "") {
+            favoritearray[counter + 1] = ({ name: itemheader.innerHTML, image: $imgholder.css("background-image"), tnimage: $tnimageholder.css("background-image") })
 
-    favoritearray.push({name: itemheader.innerHTML, image: imgholder.style.backgroundImage})
-    
-    var afavorite = document.createElement("h3");
-    afavorite.innerHTML = itemheader.innerHTML;
-    favoritesdisplay.appendChild(afavorite);
+            $favoritesdisplay.append("<h3 class ='favpanel'>" + $itemheader.html() + "</h3>");
+            $favoritesdisplay.append('<div class ="favimage" style="background-image:' + $tnimageholder.css("background-image") + ";" + 'height: 100px; width: 100px;"></div>')
 
-    var stringtostore = JSON.stringify(favoritearray);
-    localStorage.setItem("favorites", stringtostore);
-  
-    
-}
-function loadfavorites() {
-    var savedfavs = localStorage.getItem("favorites");
-    if (savedfavs != null) {
-        var loadfavs = JSON.parse(savedfavs);
-        
-        for (i = 0; i < loadfavs.length; i++) {
-            var afavorite = document.createElement("h3");
-            afavorite.innerHTML = loadfavs[i].name;
-            favoritesdisplay.appendChild(afavorite);
+            var stringtostore = JSON.stringify(favoritearray);
+            localStorage.setItem(localStorage.length + 1, stringtostore);
+
         }
-        favoritearray.concat(loadfavs)
-            
-        
     }
-}
+    // Loading saved favorites and parsing them into JSON format, then displaying their data in the favorites seciton
+    function loadfavorites() {
+        var storagekeys = [];
+        var savedfavs = [];
+        var loadfavs = [];
+        // To stop duplication if you add a favorite before loading previously saved favorites
+        $(".favpanel").remove();
+        $(".favimage").remove();
+        // get storage keys from local storage fields
+        for (i = 0; i < localStorage.length; i++) {
+            storagekeys[i] = (localStorage.key(i));
+        }
+        // puts values of localstorage fields into array
+        for (i = 0; i < storagekeys.length; i++) {
+            savedfavs[i] = (localStorage.getItem(storagekeys[i]));
+        };
 
-function clearfavorites() {
-   var h3s = document.getElementsByTagName("h3")
-   for (i = 0; i < h3s.length; i++ ){h3s[i].innerHTML = ""}
-    localStorage.removeItem("favorites");
-    favoritearray = [];
-}
+        // Jsonifiys values from saved favs array and inserts into loadfavs
+        if (savedfavs.length != 0) {
+            for (i = 0; i < savedfavs.length; i++) {
+                loadfavs[i] = jQuery.parseJSON(savedfavs[i]);
+            }
 
-function displayfavorites() {
-    for (i = 0; i < favoritearray.length; i++) {
-        var afavorite = document.createElement("h3");
-        afavorite.innerHTML = favoritearray[i].name;
-        favoritesdisplay.appendChild(afavorite);
+
+            // appends favoritesdisplay with values from json of favorited items
+            for (i = 0; i < loadfavs.length; i++) {
+                $favoritesdisplay.append("<h3 class ='favpanel'>" + loadfavs[i][0].name + "</h3>");
+                $favoritesdisplay.append('<div class ="favimage" style="background-image:' + loadfavs[i][0].tnimage + ";" + 'height: 100px; width: 100px;"></div>')
+
+            }
+
+
+
+        }
+        else { alert("You do not have any saved favorites") };
     }
-}
+    // Removes all saved data both displayed on screen and stored locally
+    function clearfavorites() {
+        localStorage.clear();
+        $(".favpanel").remove();
+        $(".favimage").remove();
+        favoritearray = [];
+        counter = -1;
+    }
+
+});
+
