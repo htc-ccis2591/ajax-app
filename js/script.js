31bfeb74d3d570ebcd941149639bf34ab30d887d
Sean Tatu <statu001@my.hennepintech.edu>
2015-11-18 15:48:54 -0600

    not complete

--- js/script.js
+++ js/script.js
@@ -1,40 +1,46 @@
 (function () {
 
-    $(document).ready(function () {
+        $(document).ready(function () {
 
 
 
-        var kevinSmithApp = {
-            loadContent: function () {
-                var movies = $("#movies");
-                $.getJSON('data/kevinSmithMovies.json', function (data) {
-                    var titles = data.titles,
-                        count = titles.length;
-                    $('<ul id=\"ul\">').insertAfter(movies.children().last());
-                    if (count > 0) {
-                        $.each(titles, function (i, movie) {
-                            $('#ul').append('<li>' + '<span>' + movie.title + ' Director: ' + movie.director + ' Producer: ' + movie.producer + ' Release Year: ' + movie.year + ' Cast: ' + movie.cast + '</span>' + '</li>');
-                        });
-                    }
+            var kevinSmithApp = {
+                loadContent: function () {
+                    var movies = $("#movies");
+                    $.getJSON('data/kevinSmithMovies.json', function (data) {
+                        var titles = data.titles,
+                            count = titles.length;
+                        $('<ul id=\"ul\">').insertAfter(movies.children().last());
+                        if (count > 0) {
+                            $.each(titles, function (i, movie) {
+                                $('#ul').append('<li>' + '<span>' + movie.title + ' Director: ' + movie.director + ' Producer: ' + movie.producer + ' Release Year: ' + movie.year + ' Cast: ' + movie.cast + '</span>' + '</li>');
+                            });
+                        }
 
-                }).error(function () {
-                    alert('unable to load titles');
-                }).complete(function () {
-                    alert('ajax call completed');
-                }).success(function () {
-                    alert('your ajax call was a success');
-                });
+                    }).error(function () {
+                        alert('unable to load titles');
+                    }).complete(function () {
+                        alert('ajax call completed');
+                    }).success(function () {
+                        alert('your ajax call was a success');
+                    });
 
-            }
-        }
-        $("#ajax").click(function () {
-            $("#movies").load(kevinSmithApp.loadContent());
+                }
+
+        };
+            $("#ajax").click(function () {
+                $("#movies").load(kevinSmithApp.loadContent());
+            });
+            $("#save").click(function () {
+               // var saveLocal = (function(){
+               // var stringObject = JSON.stringify(titles);
+                //localStorage.setItem("titles", stringObject);
+            });
+            $("#load").click(function () {
+                localStorage.loadContent();
+            });
+            $("#clear").click(function () {
+                localStorage.removeAll();
+            });
         });
-        $("#save").click(function () {
-        localStorage(titles);
-        });
-        $("#clear").click(function() {
-            storage.removeAll();
-        });
-    });
-}())
+        }())
