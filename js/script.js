1bdeaef2d239758312b2d4af1e3e66be0a4346a1
Mypeterhub <petersrun@yahoo.com>
2015-11-18 20:52:32 -0600

    first-app3 improve

--- js/script.js
+++ js/script.js
@@ -1,136 +1,194 @@
-(function () {
+//(function() {
+//var getName = $("<p>").text;
+//var ajload = $("#ajL"),
+//localload = $("#loL"),
+//localsave = $("#loS"),
+//loccalclear = $("#loC"),
 
-    //Page 172-176 in the book section Listing 8.12   
+$(document).ready(function () {
 
-    var getName = document.createElement("p");
-    var ajload = document.getElementById("ajL");
-    var localload = document.getElementById("loL");
-    var save = document.getElementById("loS");
-    var clear = document.getElementById("loC");
+    var touristSite = {
+        touristSitedata: "",
+        touristSitePlace: function (data) {
 
-    function getHTTPOject() {
-        var xhr;
+            $.ajax({
+                type: 'GET',
+                url: 'data/touristSite.json',
+                dataType: 'json',
+                // Code to run if the request succeeds;
+                // the response is passed to the function
+                success: function (data) {
+                    touristSite.touristSitedata = data;
+                    touristSite.mytouristSite(data);
+                },
+                // Code to run if the request fails; the raw request and status codes are passed to the function
+                error: function (xhr, status, errorThrown) {
+                    alert("Sorry, there was an ajax error!");
+                    console.log("Error: " + errorThrown);
+                    console.log("Status: " + status);
+                    console.dir(xhr);
+                },
 
-        if (window.XMLHttpRequest) {
-            xhr = new XMLHttpRequest();
-        } else if (window.ActiveXObject) {
-            xhr = new ActiveXObject("Msxm12.XMLHTTP");
-        }
-        return xhr;
-    }
+                // Code to run regardless of success or failure
+                complete: function (xhr, status) {
+                    alert("Your ajax call was complete!");
+                }
+            });
+        },
 
-    function ajaxcall() {
-        var gettouristSite = getHTTPOject();
-        gettouristSite.open("GET", "data/touristSite.json", true);
-        gettouristSite.send(null)
-        gettouristSite.onreadystatechange = function () {
-            //var data; 
-            if (gettouristSite.readyState === 4 && gettouristSite.status === 200) {
-                data = gettouristSite.responseText;
-                parsedata = JSON.parse(data);
-                data = parsedata;
+        mytouristSite: function (data) {
+            var item, name, loc, img, desc, email, h2, tourist, h3, text, image, para, detail, emaillink, contact, objectTarget, data;
+            console.log(data);
+            var object = data.touristSite;
+            console.log(object);
 
-                mytouristSite(parsedata);
+            var listLocation = $("#wonders");
+            var touristCount = object.length;
+            var target = $("#wonders");
+            target.innerHTML = ""
 
-            };
-        };
+            if (touristCount > 0) {
+                for (i = 0; i < touristCount; i = i + 1) {
+
+                    item = object[i];
+                    name = item.name;
+                    loc = item.location;
+                    img = item.image;
+                    desc = item.description;
+                    email = item.email;
+
+                    h2 = $("<h2>" + name + "</h2>");
+                    //tourist = document.createTextNode(name);
+                    //h2.appendChild(tourist);
+                    target.append(h2);
+
+                    h3 = $("<h3>" + loc + "</h3>");
+                    //text = document.createTextNode(loc);
+                    //h3.appendChild(text);
+                    target.append(h3);
+
+                    image = $("<img>");
+                    image.attr("src", img);
+                    target.append(image);
+                    image.attr("class", "float");
+
+                    para = $("<p>" + desc + "</p>");
+                    //detail = document.createTextNode(desc);
+                    //para.append(detail);
+                    target.append(para);
+
+                    emaillink = $("<a>");
+                    emaillink.attr("href", "mailto:" + email);
+                    emaillink.text(email);
+                    //emaillink.append(contact);
+                    target.append(emaillink);
+
+                };
+            }
+        },
+
+
+        LocalData: function () {
+            objectTarget = $("#wonders");
+            if (typeof (localStorage) === 'undefined') {
+                objectTarget.append($("<p> Sorry no local data!</p>"));
+                
+            } else {
+                objectTarget.append($("<p>Loading Data... Please take a moment!</p>"));
+                text = localStorage.getItem("myStorage");
+                if (text === null) {
+                    objectTarget.append($("<p> Sorry no local data found!</p>"));
+                } else {
+                    touristSite.touristSitedata = JSON.parse(text);
+                    console.log(touristSite.touristSitedata);
+                    touristSite.mytouristSite(touristSite.touristSitedata);
+                }
+            }
+        },
+
+        savedata: function () {
+            objectTarget = $("#wonders");
+            if (typeof (localStorage) === 'undefined') {
+                objectTarget.append($("<p> Browser does not support local storage!</p>")); 
+            } else {
+                if (localStorage === null) {
+                    objectTarget.append($("<p> Load local srorage before you save it!</p>"));
+                } else {
+                    localStorage.setItem("myStorage", JSON.stringify(touristSite.touristSitedata));
+                }
+            }
+        },
+
+        cleardata: function () {
+            if (typeof (localStorage) === 'undefined') {
+                targetArea.append($("<p> Browser does not support local storage!</p>"));
+            } else {
+                localStorage.removeItem("myStorage");
+            }
+        },
+
+        monthlyPromotions: (function (data) {
+            $("#promotions p").remove();
+            $.getJSON('data/promotions.json', function (data) {
+                var promotionsItem = data.promotions,
+                    count = promotionsItem.length;
+                if (count > 0) {
+                    $.each(promotionsItem, function (i, obj) {
+                        $('#promotions').append('<p>' + obj.description + '<p>');
+
+                    });
+                }
+
+            });
+
+        })
     };
 
-    function mytouristSite(data) {
-        var item, name, loc, img, desc, email, h2, tourist, h3, text, image, para, detail, emaillink,  contact, objectTarget, data;       
-        console.log(data);
-        var object = data.touristSite;
-        console.log(object);
+    $(document).ready(function () {
 
-        var listLocation = document.getElementById("wonders");
-        var touristCount = object.length;
-        var target = document.getElementById("wonders");
-        target.innerHTML = ""
+        var $register = $("#register"),
+            $fullname = $("#fullname"),
+            $email = $("#email");
 
-        if (touristCount > 0) {
-            for (i = 0; i < touristCount; i = i + 1) {
+        $register.submit(function () {
+            var nameValidate = $fullname.val(),
+                emailValidate = $email.val();
 
-                item = object[i];
-                name = item.name;
-                loc = item.location;
-                img = item.image;
-                desc = item.description;
-                email = item.email;
+            if (nameValidate === "" || emailValidate === "") {
 
-                h2 = document.createElement("h2");
-                tourist = document.createTextNode(name);
-                h2.appendChild(tourist);
-                target.appendChild(h2);
+                alert("All fields can not be blank!");
 
-                h3 = document.createElement("h3");
-                text = document.createTextNode(loc);
-                h3.appendChild(text);
-                target.appendChild(h3);
+            } else if (nameValidate.length < 2) {
 
-                image = document.createElement("img");
-                image.setAttribute("src", img);
-                target.appendChild(image);
-                image.setAttribute("class", "float");
+                alert("Need a valid name at least 2 charactors!");
 
-                para = document.createElement("p");
-                detail = document.createTextNode(desc);
-                para.appendChild(detail);
-                target.appendChild(para);
+            } else if (!validateEmail(emailValidate)) {
 
-                emaillink = document.createElement("a");
-                emaillink.setAttribute("href", "mailto:" + email);
-                contact = document.createTextNode(email);
-                emaillink.appendChild(contact);
-                target.appendChild(emaillink);
-
-            };
-        }
-    }
-
-    objectTarget = document.getElementById("wonders");
-
-    function LocalData() {
-        if (typeof (localStorage) === 'undefined') {
-            objectTarget.innerHTML = "sorry!";
-        } else {
-            objectTarget.innerHTML = "Loading Data... Please take a moment.";
-            text = localStorage.getItem("myStorage");
-            if (text === null) {
-                objectTarget.innerHTML = "no local data found.";
-            } else {
-                data = JSON.parse(text);
-                console.log(data);
-                mytouristSite(data);
+                alert("Email is invalid!");
             }
-        }
-    }
-    data = null
 
-    function savedata() {
-        if (typeof (localStorage) === 'undefined') {
-            objectTarget.innerHTML = "Browser does not support local storage";
-        } else {
-            if (localStorage === null) {
-                objectTarget.innerHTML = "Load local srorage before you save it.";
-            } else {
-                localStorage.setItem("myStorage", JSON.stringify(data));
-                console.log(data);
+            function validateEmail(emailValidate) {
+                var emailReg = new RegExp(/^[\w-.+]+@[a-zA-Z0-9.-]+.[a-zA-z0-9]{2,4}$/),
+                    valid = emailReg.test(emailValidate);
+
+                if (valid) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
-        }
-    }
 
-    function cleardata() {
-        if (typeof (localStorage) === 'undefined') {
-            targetArea.innerHTML = " Browser does not support local storage.";
-        } else {
-            localStorage.removeItem("myStorage");
-        }
-    }
+        });
+
+    });
+
+    $("#ajL").click(touristSite.touristSitePlace);
+
+    $("#loL").click(touristSite.LocalData);
+
+    $("#loS").click(touristSite.savedata);
+
+    $("#loC").click(touristSite.cleardata);
 
 
-    ajload.addEventListener("click", ajaxcall, false);
-    localload.addEventListener("click", LocalData, false);
-    save.addEventListener("click", savedata, false);
-    clear.addEventListener("click", cleardata, false);
-
-})();
+}());
