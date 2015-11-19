8108d7f12e7e4d1a9a53b1d8c903b8a68090ac0f
Sean Tatu <statu001@my.hennepintech.edu>
2015-11-18 20:32:33 -0600

    buttons work!!!

--- js/script.js
+++ js/script.js
@@ -1,22 +1,15 @@
 (function () {
 
-        $(document).ready(function () {
-
+        $(document).ready(function () { 
 
 
             var kevinSmithApp = {
+                localStorageKey:"saveMovies",
+                movieData:"",
                 loadContent: function () {
-                    var movies = $("#movies");
                     $.getJSON('data/kevinSmithMovies.json', function (data) {
-                        var titles = data.titles,
-                            count = titles.length;
-                        $('<ul id=\"ul\">').insertAfter(movies.children().last());
-                        if (count > 0) {
-                            $.each(titles, function (i, movie) {
-                                $('#ul').append('<li>' + '<span>' + movie.title + ' Director: ' + movie.director + ' Producer: ' + movie.producer + ' Release Year: ' + movie.year + ' Cast: ' + movie.cast + '</span>' + '</li>');
-                            });
-                        }
-
+                        kevinSmithApp.movieData = data;
+                        kevinSmithApp.buildHTML();
                     }).error(function () {
                         alert('unable to load titles');
                     }).complete(function () {
@@ -25,22 +18,39 @@
                         alert('your ajax call was a success');
                     });
 
+                },
+                buildHTML: function(){
+                    var movies = $("#movies");
+                    movies.empty();
+                    var titles = kevinSmithApp.movieData.titles,
+                            count = titles.length;
+                    movies.append ($('<ul id=\"ul\">'));
+                        if (count > 0) {
+                            $.each(titles, function (i, movie) {
+                                $('#ul').append('<li>' + '<span>' + movie.title + ' Director: ' + movie.director + ' Producer: ' + movie.producer + ' Release Year: ' + movie.year + ' Cast: ' + movie.cast + '</span>' + '</li>');
+                            });
+                        }
+
                 }
+                
 
         };
             $("#ajax").click(function () {
                 $("#movies").load(kevinSmithApp.loadContent());
             });
             $("#save").click(function () {
-               // var saveLocal = (function(){
-               // var stringObject = JSON.stringify(titles);
-                //localStorage.setItem("titles", stringObject);
+                //var saveLocal = (function(){
+                var string = JSON.stringify(kevinSmithApp.movieData);
+                localStorage.setItem(kevinSmithApp.localStorageKey, string);
             });
             $("#load").click(function () {
-                localStorage.loadContent();
+                var string = localStorage.getItem(kevinSmithApp.localStorageKey);
+                kevinSmithApp.movieData = JSON.parse(string);
+                kevinSmithApp.buildHTML();
+                
             });
             $("#clear").click(function () {
-                localStorage.removeAll();
+                localStorage.removeItem(kevinSmithApp.localStorageKey);
             });
         });
         }())
